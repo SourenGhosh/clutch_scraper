6d9be2f7fd9508d0dadd364e74d20f6db72feebd
souren ghosh <souren94ghosh@gmail.com>
2022-09-09 20:29:56 +0530

    webdriver

--- scraper.py
+++ scraper.py
@@ -1,13 +1,163 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup as bs
-
-base_url = "https://clutch.co/directory/mobile-application-developers"
-
+from webdriver import get_service_location, find_firm_contact
 
 
-with requests.session() as s:
-    soup = bs(s.post(base_url).content, "html.parser")
-    links = soup.find_all("li", attrs={"class": "provider provider-row sponsor"})
-    for link in links:
-        name = link.find("a", attrs={"data-link_text": "Profile Title"})
-    print(name.text.strip())
+BASE_URL = "https://clutch.co"
+base_url = "https://clutch.co/directory/mobile-application-developers?page=1"
+
+class BaseScraper(object):
+    def __init__(self, base_url, *args, **kwargs):
+        self.base_url = base_url
+
+    def soup_base_session(self, url):
+        with requests.session() as s:
+            soup = bs(s.post(url).content, "html.parser")
+        return soup
+
+    @property
+    def clutch_list_of_services(self):
+        services = list()
+        soup_obj = self.soup_base_session(self.base_url)
+        links = soup_obj.find_all("a", attrs={"class": "facets_list__item"})
+        for link in links:
+            service = link.find("span", attrs={"class": "facets_list__item__name"}).find('span', attrs={
+                "class": "name"
+            })
+            service_data_url = link.find("input")
+            services.append(
+                {
+                    'service_name': service.text.strip(),
+                    'data_url': service_data_url['data-url']
+                } 
+            )
+        print(services)
+        return services
+
+    def get_last_page(self, data_url):
+        url = "%s%s" %(self.base_url, data_url)
+        print(url)
+        soup_obj = self.soup_base_session(url)
+        link = soup_obj.find("li", attrs={"class": "page-item last"}).find('a')
+        last_data_page = link['data-page']
+        print(last_data_page)
+        return last_data_page
+
+    def get_available_location(self, service):
+        return get_service_location(self.base_url, service)
+    
+    def find_service_data_url(self, service_name):
+        service_data_url = None
+        for service in self.clutch_list_of_services:
+            if service['service_name'] == service_name:
+                service_data_url = service['data_url']
+        return service_data_url
+
+    def get_location_id_from_name(self, location_name, service_name):
+        data = self.get_available_location(service_name)
+        loc_id=None
+        print(".................", list(data))
+        datas=list(data)
+        for d in datas:
+            print(d)
+            if location_name in d[0]:
+                loc_id = d[1]
+        print(loc_id)
+        return loc_id
+
+
+
+
+    def scrape_data(self, service_name, location=None, page_no=False):
+        service_data_url = self.find_service_data_url(service_name)
+        service_last_page = self.get_last_page(service_data_url)
+        if service_data_url is None:
+            raise Exception("No data Url found for the service name")
+        service_url = f"{self.base_url}{service_data_url}"
+
+        if location is not None:
+            loc_id = get_location_id_from_name(location)
+            service_url = f"{self.base_url}{service_data_url}?geona_id={loc_id}"
+
+        if page_no:
+            last_page=service_last_page
+        else:
+            last_page=1
+        data_list = list()
+        for page in range(last_page):
+            service_url = f"{service_url}?page={page}"
+            soup_obj = self.soup_base_session(service_url)
+            links = soup_obj.find_all("li", attrs={"class": "provider provider-row sponsor"})
+            for link in links:
+                name = link.find("a", attrs={"data-link_text": "Profile Title"})
+                company_url = link.find("a", attrs={"class": "website-link__item"})
+                locality = link.find("span", attrs={"class": "locality"})
+                rating = link.find("span", attrs={"class": "rating sg-rating__number"})
+                rating_count = link.find('a', attrs={'data-link_text': "Reviews Count"})
+                hourly_rate = link.find('div', attrs={'data-content': "<i>Avg. hourly rate</i>"}).find('span')
+                min_project_size = link.find('div', attrs={'data-content': "<i>Min. project size</i>"}).find('span')
+                employee_size = link.find('div', attrs={'data-content': "<i>Employees</i>"}).find('span')
+                company_data_url = name['href']
+                profile_url = f"{self.base_url}{company_data_url}#summary"
+                print(profile_url)
+                contact_no = find_firm_contact(profile_url)
+
+                print(contact_no)
+
+            data_list.append(
+                {
+                    "name": name.text.strip(),
+                    "company_url": company_url['href'],
+                    "locality": locality.text.strip(),
+                    "rating": rating.text.strip(),
+                    "hourly_rate": hourly_rate.text.strip(),
+                    "min_project_size": min_project_size.text.strip(),
+                    "employee_size": employee_size.text.strip(),
+                    "contact": contact_no
+                }
+            )
+        df = pd.DataFrame.from_dict(data_list)
+        df.to_csv('temp.csv')
+
+
+
+
+
+# with requests.session() as s:
+#     soup = bs(s.post(base_url).content, "html.parser")
+#     links = soup.find_all("li", attrs={"class": "provider provider-row sponsor"})
+#     for link in links:
+#         name = link.find("a", attrs={"data-link_text": "Profile Title"})
+#         company_url = link.find("a", attrs={"class": "website-link__item"})
+#         locality = link.find("span", attrs={"class": "locality"})
+#         rating = link.find("span", attrs={"class": "rating sg-rating__number"})
+#         rating_count = link.find('a', attrs={'data-link_text': "Reviews Count"})
+#         hourly_rate = link.find('div', attrs={'data-content': "<i>Avg. hourly rate</i>"}).find('span')
+#         min_project_size = link.find('div', attrs={'data-content': "<i>Min. project size</i>"}).find('span')
+#         employee_size = link.find('div', attrs={'data-content': "<i>Employees</i>"}).find('span')
+#         details_url = f"https://clutch.co/profile/{name.text.strip().lower()}/#summary"
+#         company_dt_soup = bs(s.post(details_url).content, "html.parser")
+        
+#         contact = company_dt_soup.find_all("h3", attrs={"class": "error-type"})
+
+    
+#     print(name.text.strip())
+#     print(company_url['href'])
+#     print(locality.text.strip())
+#     print(rating.text.strip())
+#     print(rating_count.text.strip())
+#     print(hourly_rate.text.strip())
+#     print(min_project_size.text.strip())
+#     print(employee_size.text.strip())
+#     print(details_url)
+#     print(contact)
+
+
+base_obj = BaseScraper(BASE_URL)
+# base_obj.clutch_list_of_services
+# base_obj.get_last_page('/directory/mobile-application-developers')
+# print(base_obj.get_available_location('Mobile App Development'))
+base_obj.scrape_data('Mobile App Development')
+
+#base_obj.get_location_id_from_name('Gurgaon', 'Mobile App Development')
---
+++ webdriver.py
@@ -0,0 +1,76 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import FirefoxProfile
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+firefox_options = Options()
+firefox_options.add_argument("--disable-infobars")
+firefox_options.add_argument("--disable-extensions")
+firefox_options.add_argument("--disable-popup-blocking")
+
+profile_options = FirefoxProfile()
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.5; rv:90.0) Gecko/20100101 Firefox/90.0'
+profile_options.set_preference('profile_options = FirefoxProfile()', user_agent)
+profile_options.set_preference("print_printer", "Mozilla Save to PDF")
+profile_options.set_preference("print.always_print_silent", True)
+profile_options.set_preference("print.show_print_progress", False)
+profile_options.set_preference('print.save_as_pdf.links.enabled', True)
+profile_options.set_preference("print.printer_Mozilla_Save_to_PDF.print_to_file", True)
+
+driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver', options=firefox_options,
+                           firefox_profile=profile_options)
+
+
+
+# URL = "https://clutch.co/profile/naked#summary"
+
+# driver.get(URL)
+
+# x=driver.find_element(By.XPATH, "//*[@class='tel']/.//span[contains(@class,'value')]")
+# print(x.get_attribute('innerHTML'))
+
+def find_firm_contact(url):
+    driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver', options=firefox_options,
+                           firefox_profile=profile_options)
+
+    driver.get(url)
+    x=driver.find_element(By.XPATH, "//*[@class='tel']/.//span[contains(@class,'value')]")
+    print(".................", x.get_attribute('innerHTML'))
+    contact = x.get_attribute('innerHTML')
+    driver.close()
+    return contact
+
+
+def get_service_location(url, service):
+    driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver', options=firefox_options,
+                           firefox_profile=profile_options)
+
+    driver.get(url)
+    service_input =  driver.find_element_by_id("services")
+    #service_input.send_keys(service)
+    search_span = f"//span[contains(., '{service}')]"
+    driver.find_element_by_css_selector("input[aria-controls='services_dropdown']").click()
+    sleep(5)
+    driver.find_element(By.XPATH, "//span[contains(text(),'Mobile App Development')]").click();
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@class='facets_list__item__name']/.//span[contains(.,'Mobile App Development')]"))).click()
+    #x=driver.find_element(By.XPATH, "//*[@class='facets_list facets_list__location']/.//span[contains(@class,'name')]");
+    sleep(7)
+    x=driver.find_elements_by_xpath('//div[contains(@id, "location_list")]//a[contains(@href, "#")]')
+    location_list = [i.get_attribute('aria-label') for i in x]
+    ids=driver.find_elements_by_xpath('//input[contains(@name, "location")]')
+    location_id_list = [id.get_attribute('value') for id in ids]
+    location_id_list.pop(0)
+    print(location_list)
+    print(location_id_list)
+    zipped_location = zip(location_list, location_id_list)
+    driver.close()
+    return zipped_location
+
+
+if __name__ == "__main__":
+    get_service_location("https://clutch.co", "Mobile App Development")
